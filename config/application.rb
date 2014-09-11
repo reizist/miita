74c3d17675cfa81812b8d0ee391835d82c4d3dc3
reizist <reizist@gmail.com>
2014-09-12 00:24:17 +0900

    Config generators

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,19 @@ module AmedayoWeb
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :haml
+      g.test_framework  :rspec, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.view_specs false
+      g.controller_specs false
+      g.routing_specs false
+      g.helper_specs false
+      g.request_specs false
+      g.assets false
+      g.helper false
+    end
   end
 end
