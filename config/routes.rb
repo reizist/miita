d108ff9b5482eed159a1f67779d6e0e693c77e7d
reizist <reizist@gmail.com>
2015-01-18 00:44:06 +0900

    Rails g scaffold_controller articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'statics#home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :articles
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
