c6e49c42d6117dd99822cf3428c5d86fd33685ab
reizist <reizist@gmail.com>
2015-01-19 01:49:36 +0900

    Implement rendering as markdown if the end of the article title is '.md'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def diff(old_str, new_str, options = {})
     Diffy::Diff.new(old_str, new_str, options)
   end
+
+  def markdown(content)
+    GitHub::Markdown::render(content).html_safe
+  end
+
+  # タイトルの終端が'.md'で終わってたらmarkdownとしてrender
+  def markdown?(title)
+    /^*\.md$/ === title
+  end
 end
