82fa2922078d27e3f0b095ac278e0ba8e43bb05d
reizist <reizist@gmail.com>
2016-01-10 06:40:58 +0900

    Implement pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.page(params[:page])
   end
 
   # GET /articles/1
