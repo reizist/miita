15ee0da6a83396d8394a6642f1497f3c4373e421
reizist <reizist@gmail.com>
2016-01-11 02:17:19 +0900

    Implement article search func

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,11 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def search
+    @articles = Article.search params[:query], fields: [:title, :content]
+
+  end
+
   # GET /articles/1
   # GET /articles/1.json
   def show
